7da7d31bedb2d728942d4d5211b7e38ef5bb3ff9
Vivek9874 <59100215+Vivek9874@users.noreply.github.com>
2024-07-14 16:43:51 +0530

    Pushed all the files with the documentation

---
+++ app/src/main/java/com/viv/videomeeting/listeners/UsersListener.java
@@ -0,0 +1,12 @@
+package com.viv.videomeeting.listeners;
+
+import com.viv.videomeeting.models.User;
+
+public interface UsersListener {
+
+    void initiateVideoMeeting(User user);
+
+    void initiateAudioMeeting(User user);
+
+    void onMultipleUsersAction(Boolean isMultipleUsersSelected);
+}
